610632734a4fdd1bc2c87283fa73176ba5f3b3d8
exolotyll <166331205+exolotyll@users.noreply.github.com>
2024-06-23 23:48:11 -0400

    gravy boat

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
         };
     };
 
-    outputs = { nixpkgs, home-manager }: let
+    outputs = { nixpkgs, home-manager, ... }: let
         system = "x86_64-linux";
         pkgs = nixpkgs.legacyPackages.${system};
     in {
